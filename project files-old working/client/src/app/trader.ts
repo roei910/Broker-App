d2efbed2b36eb8b91aae00756ec3abc3ed6ee930
roei910 <74923231+roei910@users.noreply.github.com>
2023-08-06 11:14:30 +0300

    removed old files

--- project files-old working/client/src/app/trader.ts
+++
@@ -1,6 +0,0 @@
-export interface Trader {
-    Id: number,
-    Name: string,
-    Money: number,
-    Shares: any
-}
--- project files-old working/client/src/main.ts
+++
@@ -1,8 +0,0 @@
-import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
-
-import { AppModule } from './app/app.module';
-
-var appData;
-
-platformBrowserDynamic().bootstrapModule(AppModule)
-  .catch(err => console.error(err));
